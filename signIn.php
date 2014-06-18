98ea738c2ce6e2527af753f16495a76650ef2364
Jared Wadsworth <wad09007@byui.edu>
2014-06-17 20:47:22 -0600

    added images

---
+++ signIn.php
@@ -0,0 +1,17 @@
+<?php
+$email = $_POST("email");
+$pass  = $_POST("pass");
+
+echo $email;
+echo " : " + $pass;
+
+$con = mysql_connect("localhost","FTselect","select");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+
+
+mysql_close($con);
+?>
