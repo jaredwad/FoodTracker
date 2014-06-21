8c4bf3e37d651a4f10ab9602691744ccef4e66bc
Jared Wadsworth <wad09007@byui.edu>
2014-06-21 14:44:15 -0600

    fixed insert

--- resetDB.sql
+++ resetDB.sql
@@ -2,10 +2,24 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user
 ( user_id int not null AUTO_INCREMENT
+, first_name varchar(30) not null
+, middle_name varchar(30)
+, last_name varchar(30) not null
 , email varchar(30) not null
 , pass varchar(30) not null
 , token varchar(30)
 , last_login date
 , PRIMARY KEY ( user_id ));
 
-insert into user (email, pass, token) values ('wad09007@byui.edu', 'password', 'aaaa');
+insert into user 
+( first_Name
+, last_Name
+, email
+, pass
+, token) 
+values 
+( 'Jared'
+, 'Wadsworth'
+, 'wad09007@byui.edu'
+, 'password'
+, 'aaaa');
