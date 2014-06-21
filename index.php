7d1bdb2200498f82ce72a8d68897dda24109dc01
Jared Wadsworth <wad09007@byui.edu>
2014-06-21 15:36:55 -0600

    moved session php

--- index.php
+++ index.php
@@ -1,35 +1,7 @@
-<?php session_start(); ?>
-
-<html>
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Jared Wadsworth homepage">
-    <meta name="author" content="Jared Wadsworth">
-    <link rel="shortcut icon" href="images/wheat-icon.ico">
-
-    <title>FoodTracker login</title>
-
-    <!-- Custom styles for this template -->
-    <link href="css/signin.css" rel="stylesheet">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</head>
-
-<body>
-    
-    <?php
+<?php
+        //Start the session
+        session_start();
+        
         // checks if the the user came here through a post (most likely itself)
         if (!empty($_POST["email"]) && !empty($_POST["pass"])) {
             // create connection
@@ -75,6 +47,35 @@
         mysqli_close($mysqli);
         
     ?>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Jared Wadsworth homepage">
+    <meta name="author" content="Jared Wadsworth">
+    <link rel="shortcut icon" href="images/wheat-icon.ico">
+
+    <title>FoodTracker login</title>
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
 
     <div class="container">
 
