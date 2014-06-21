13e2f155068c5aeb640c252f4093463175190ae2
Jared Wadsworth <wad09007@byui.edu>
2014-06-21 14:41:45 -0600

    added drop table functionality, and inserted test user

--- resetDB.sql
+++ resetDB.sql
@@ -1,9 +1,11 @@
+DROP TABLE IF EXISTS user;
 
-
-create table user
+CREATE TABLE user
 ( user_id int not null AUTO_INCREMENT
 , email varchar(30) not null
 , pass varchar(30) not null
 , token varchar(30)
 , last_login date
 , PRIMARY KEY ( user_id ));
+
+insert into user (email, pass, token) values ('wad09007@byui.edu', 'password', 'aaaa');
