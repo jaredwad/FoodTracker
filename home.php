4bcb5f6d1ad546d984f8d2babaa921fa48cee122
Jared Wadsworth <wad09007@byui.edu>
2014-06-24 18:32:46 -0600

    test

--- home.php
+++ home.php
@@ -83,6 +83,10 @@
     <button class="btn btn-primary btn-lg" id="modalButton">
         Launch demo modal
     </button>
+    <!-- Button trigger modal -->
+    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+        Launch demo modal
+    </button>
 
     <!-- Modal -->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
