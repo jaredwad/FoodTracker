b3565fd26f0ce215de596a4db4826449f949d688
Jared Wadsworth <wad09007@byui.edu>
2014-06-24 18:49:30 -0600

    test

--- home.php
+++ home.php
@@ -84,22 +84,34 @@
 </head>
 
 <body>
-    <button class="btn btn-primary btn-lg" id="modalButton">
+    <!-- Button trigger modal -->
+    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
         Launch demo modal
     </button>
-    <!-- Button trigger modal -->
-    <a class="btn" data-toggle="modal" href="#myModal">Launch Modal</a>
 
-
-    <div class="modal fade" id="myModal">
-        <p>some content</p>
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="container">
 
-        <!--Div that will hold the pie chart-->
-        <!--        <div id="chart_div" class="centeredPie"></div>
+    <!--Div that will hold the pie chart-->
+    <!--        <div id="chart_div" class="centeredPie"></div>
 -->
-        <!-- Button trigger modal -->
+    <!-- Button trigger modal -->
 
     </div>
 </body>
