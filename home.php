49aaeb56f0b0ee104fb8c661946f45e3d5fd95ea
Jared Wadsworth <wad09007@byui.edu>
2014-06-24 20:10:11 -0600

    test

--- home.php
+++ home.php
@@ -20,9 +20,6 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="home.css">
 
-    <!-- Minified JQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-
     <!-- Latest compiled and minified JavaScript -->
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
