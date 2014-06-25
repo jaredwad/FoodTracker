263c90063cd2764a0d05e2e0b1569772a816de18
Jared Wadsworth <wad09007@byui.edu>
2014-06-24 18:55:14 -0600

    fixed jquery

--- home.php
+++ home.php
@@ -27,7 +27,7 @@
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
     <!-- Minified JQuery -->
-    <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
 
     <!--Load the AJAX API-->
