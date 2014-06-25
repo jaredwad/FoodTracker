c5f5825b0620c594b815e4978e1d9d4d74fdf62c
Jared Wadsworth <wad09007@byui.edu>
2014-06-24 19:41:59 -0600

    added chart back in and took out the modal

--- home.php
+++ home.php
@@ -85,34 +85,9 @@
 
     <div id="example" href="#" data-content="This is the content for the popover.">Your Popover Text Here.</div>
 
-
-    <!-- Button trigger modal -->
-    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-        Launch demo modal
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <!--Div that will hold the pie chart-->
-    <!--        <div id="chart_div" class="centeredPie"></div>
--->
+    <div id="chart_div" class="centeredPie"></div>
+
     <!-- Button trigger modal -->
 
     </div>
