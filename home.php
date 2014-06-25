a28b55f16ddb21903f9d951b4227fe331dc32d5d
Jared Wadsworth <wad09007@byui.edu>
2014-06-24 18:35:50 -0600

    tried a different method of modal

--- home.php
+++ home.php
@@ -84,28 +84,24 @@
         Launch demo modal
     </button>
     <!-- Button trigger modal -->
-    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-        Launch demo modal
-    </button>
+    <a class="btn" data-toggle="modal" href="#myModal">Launch Modal</a>
 
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-                </div>
-                <div class="modal-body">
-                    <p>Hello!</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
+
+    <div class="modal hide" id="myModal">
+        <!-- note the use of "hide" class -->
+        <div class="modal-header">
+            <button class="close" data-dismiss="modal">×</button>
+            <h3>Modal header</h3>
         </div>
-    </div>
+        <div class="modal-body">
+            <p>One fine body…</p>
+        </div>
+        <div class="modal-footer">
+            <a href="#" class="btn" data-dismiss="modal">Close</a>
+            <!-- note the use of "data-dismiss" -->
+            <a href="#" class="btn btn-primary">Save changes</a>
+        </div>
+    </div>​
     <div class="container">
 
         <!--Div that will hold the pie chart-->
