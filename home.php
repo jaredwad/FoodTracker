cc5e99a593d8de32f7382e6ddb99a58d22adcc54
Jared Wadsworth <wad09007@byui.edu>
2014-06-24 19:55:30 -0600

    testing on click functionality

--- home.php
+++ home.php
@@ -37,35 +37,42 @@
          // Set a callback to run when the Google Visualization API is loaded.
         google.setOnLoadCallback(drawChart);
 
-         // Callback that creates and populates a data table,
+         // Callback that creates and populates a data table, 
          // instantiates the pie chart, passes in the data and
          // draws it.
         function drawChart() {
 
             // Create the data table.
             var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Name');
-            data.addColumn('number', 'Amount');
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
             data.addRows([
-                         ['Wheat', 3],
-                         ['Fruit', 1],
-                         ['Vegtables', 1],
-                         ['Other', 1],
-                         ['Pepperoni', 2]
-                                        ]);
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
 
             // Set chart options
             var options = {
-                //                'title': "<?//php echo $_SESSION['first_name'] . "'s Stroage Chart"; ?>",
-                'width': 1000,
-                'height': 800,
-                'is3D': true,
-                'colors': ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-                'enableInteractivity': true
+                'title': 'How Much Pizza I Ate Last Night',
+                'width': 400,
+                'height': 300
             };
 
             // Instantiate and draw our chart, passing in some options.
             var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+
+            function selectHandler() {
+                var selectedItem = chart.getSelection()[0];
+                if (selectedItem) {
+                    var topping = data.getValue(selectedItem.row, 0);
+                    alert('The user selected ' + topping);
+                }
+            }
+
+            google.visualization.events.addListener(chart, 'select', selectHandler);
             chart.draw(data, options);
         }
     </script>
