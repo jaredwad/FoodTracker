77f9233b26ff5d483bc8e0c026daec3b5576aa61
Jared Wadsworth <wad09007@byui.edu>
2014-06-25 17:15:08 -0600

    changed all the variable names

--- home.php
+++ home.php
@@ -1,4 +1,12 @@
-<?php session_start(); if (!isset($_SESSION[ 'user_id']) && !empty($_POST["user_id"])) { header( 'Location: index.php'); } ?>
+<?php session_start();
+    if (!isset($_SESSION[ 'user_id']) && !empty($_SESSION['user_id'])) { 
+        header( 'Location: index.php');
+    } 
+
+    $query = "SELECT * FROM user WHERE email ='" . $_SESSION['user_id'] . "'";
+                
+    $result = mysqli_query($con, $query);
+?>
 
 
 <html>
@@ -37,42 +45,52 @@
         google.setOnLoadCallback(drawChart);
 
         function drawChart() {
-            
+
             /*****  Get all the variables needed for this chart *****/
-            
+
             /*** Size (lbs) ***/
-            var wheatSize = getWheatSize();
-            var fruitSize = getFruitSize();
-            var vegtablesSize = getVegtablesSize();
-            var otherSize = getOtherSize();
+            var grainsSize  = 210;
+            var fatsSize    = 210;
+            var legumesSize = 210;
+            var sugarsSize  = 210;
+            var milkSize    = 210;
+            var cookingSize = 210;
+            var waterSize   = 210;
+
             
             /*** Colors ***/
-            var wheatColor = getWheatColor(wheatSize);
-            var fruitColor = getFruitColor(fruitSize);
-            var vegtablesColor = getVegtablesColor(vegtablesSize);
-            var otherColor = getOtherColor(otherSize);
-            
+            var grainsColor  = 'red';
+            var fatsColor    = 'red';
+            var legumesColor = 'red';
+            var sugarsColor  = 'red';
+            var milkColor    = 'red';
+            var cookingColor = 'red';
+            var waterColor   = 'red';
+
             //Global variable!!! Good thing Brother Helfrich won't be looking at my code
             data = google.visualization.arrayToDataTable([
       ['Food', 'Percentage of total storage (lbs)'],
-      ['Wheat', 210],
-      ['Fruit', 200],
-      ['Vegtables', 175],
-      ['Other', 15]
+      ['Grains', grainsSize],
+      ['Fats and Oils', fatsSize],
+      ['Legumes', legumesSize],
+      ['Sugars', sugarsSize],
+      ['Milk', milkSize],
+      ['Cooking Essentials', cookingSize],
+      ['Water', waterSize]
     ]);
 
             //Global variable!!! Good thing Brother Helfrich won't be looking at my code
             chart = new google.visualization.PieChart(
                 document.getElementById('piechart'));
 
-            
+
             //All of the desired options for the pie chart
             var options = {
                 legend: 'none',
                 pieSliceText: 'label',
-                colors: [wheatColor, fruitColor, vegtablesColor, otherColor],
+                colors: [grainsColor, fatsColor, legumesColor, sugarsColor, milkColor, cookingColor, waterColor],
                 pieSliceBorderColor: 'black',
-                pieStartAngle: 100,
+                pieStartAngle: 30,
                 tooltip: {
                     isHtml: true
                 }
@@ -84,44 +102,42 @@
                 //check row with e.row;                                
                 $(".google-visualization-tooltip").html(HTMLContent(e));
             });
-            
+
             //This prevents the pie chart from reverting to default tooltip content
             //when clicked
-            google.visualization.events.addListener(chart, 'select', function (e) { 
+            google.visualization.events.addListener(chart, 'select', function (e) {
                 //check row with e.row;
                 e = chart.getSelection();
                 $(".google-visualization-tooltip").html(HTMLContent(e[0]));
-            }
-            );
+            });
 
             chart.draw(data, options);
         }
 
         function HTMLContent(e) {
             var FoodType = data.getFormattedValue(e.row, 0);
-            
+
             return '<div style="padding:5px 5px 5px 5px;"> html content! ' + FoodType + '</div>';
         }
     </script>
 
     <script>
-        
-        function getWheatColor() {
-            return 'red';   
+        function getGrainsColor(grainsSize) {
+            return 'red';
         }
-        
-        function getFruitColor() {
-            return 'red';   
+
+        function getFatsColor(fatsSize) {
+            return 'red';
         }
-        
-        function getVegtablesColor() {
-            return 'red';   
+
+        function getLegumesColor(legumesSize) {
+            return 'red';
         }
-        
-        function getOtherColor() {
-            return 'red';   
+
+        function getSugarsColor(sugarsSize) {
+            return 'red';
         }
-        
+
         $(document).ready(function () {
             $('#example').popover({
                 trigger: "hover",
