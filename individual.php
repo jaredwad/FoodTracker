8016f253473a7d8f9e0a35874cfaba33485ccca4
Jared Wadsworth <wad09007@byui.edu>
2014-06-26 03:36:03 -0600

    test

--- individual.php
+++ individual.php
@@ -31,7 +31,7 @@
 
     $query = "SELECT * FROM " . lcfirst($table) . " WHERE user_id ='" . $_SESSION['user_id'] . "'";
 
-   echo $query;
+//   echo $query;
                 
     $set = mysqli_query($con, $query);
     $totalSize = 0;
