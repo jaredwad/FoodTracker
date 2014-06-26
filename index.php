7097ce448190628bf23c00dad25fbc08ddfe8873
Jared Wadsworth <wad09007@byui.edu>
2014-06-26 04:25:43 -0600

    no checkbox

--- index.php
+++ index.php
@@ -95,9 +95,6 @@
             <h2 class="form-signin-heading">Please Sign In</h2>
             <input name="email" type="email" class="form-control" placeholder="Email address" required autofocus>
             <input name="pass" type="password" class="form-control" placeholder="Password" required>
-            <label class="checkbox">
-                <input type="checkbox" value="remember-me">Remember me
-            </label>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         </form>
 
