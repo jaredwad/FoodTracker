2ea6542dadda1544588bae5ed89043d51b007264
Jared Wadsworth <wad09007@byui.edu>
2014-07-30 15:32:28 -0600

    eliminated unnecessary overhead for add.php

--- add.php
+++ add.php
@@ -26,30 +26,8 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Jared Wadsworth">
-    <link rel="shortcut icon" href="images/wheat-icon.ico">
-    <title>FoodTracker
-        <?php echo $_POST[ 'type']; ?>
-    </title>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="individual.css">
-
     <!-- Minified JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
     <script type="text/javascript">
         $(document).ready(function () {
